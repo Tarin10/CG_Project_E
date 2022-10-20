29efbd009bf34fec8008f89596814b3ad49994aa
Tarin10 <114634381+Tarin10@users.noreply.github.com>
2022-10-20 10:10:20 +0600

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int array[4]={2,5,6,3};
+   int n,i;
+   for(int i=0; i<n-1; i++)
+     {
+      cout<<"The iteration Number is: "<<endl;
+     }
+    for(int j=0; i<n-i-1; i++)
+    {
+        if(array[j]<array[j+1])
+        {
+           cout<<"array[j]"<<j<<endl;
+           break;
+        }
+     cout<<"The index number is: "<<endl;
+    }
+    cout<<i<<endl;
+    return 0;
+}
