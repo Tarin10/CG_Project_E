317bc93400711704ca8aa23dd76c6c9d1c7c7cb1
Tarin10 <114634381+Tarin10@users.noreply.github.com>
2023-02-06 10:31:36 +0600

    Add files via upload

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,29 +1,55 @@
 #include<iostream>
 using namespace std;
 
-void print (int array[], int x)
+
+void bubbleSort(int array[], int n)
 {
-    cout<<"\nEnter the Array =";
-    for(int i=0; i<x-1; i++)
+    int countP = 0;
+    int comp = 0;
+    int swapp = 0;
+
+    for(int pass = 1; pass < n; pass++)
     {
-        cout<<array[i];
-        if(x<=0)
+        countP++;
+        cout<<" The number of Pass : "<<pass<<endl;
+
+        bool flag= true;
+        for(int i=0; i< n-pass; i++)
         {
-            cout<<"/n";
-        }
-        else
-        {
-            cout<<array[x-1];
+            cout<<"Comparing : "<<array[i]<<" & "<<array[i+1]<<endl;
+
+            comp++;
+
+           if(array[i] > array[i+1])
+            {
+                cout<<"Swapping : "<<array[i]<<" & "<<array[i+1]<<endl;
+                swap(array[i],array[i+1]);
+
+                flag = false;
+                swapp++;
+
+            }
+
         }
 
+
+        if(flag)break;
+        countP++;
     }
+
+     cout<<"\n Total Pass : "<<countP<<endl;
+     cout<<"\n Total Comparison : "<<comp<<endl;
+     cout<<"\n Total Swapping : "<<swapp<<endl;
+
+
+
 }
 
 void insertionSort(int array[], int n)
 {
     int i,j,key;
     int comp = 0;
-    int swap = 0;
+    int swapp = 0;
     for(i=1; i<n; i++)
     {
         key = array[i];
@@ -33,19 +59,24 @@ void insertionSort(int array[], int n)
             comp++;
             array[j+1] = array[j];
             j= j-1;
-            swap++;
+            swapp++;
         }
         array[j+1] = key;
     }
     cout<<"\n Total Comparison :"<<comp<<endl;
-    cout<<"\n Total Swapping :"<<swap<<endl;
+    cout<<"\n Total Swapping :"<<swapp<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
 }
 
 int main()
 {
     int array[10] = {33,66,22,99,77,11,91,55,88,44};
+    bubbleSort(array, 10);
     insertionSort(array, 10);
-    print(array,10);
+
     return 0;
 
 }
